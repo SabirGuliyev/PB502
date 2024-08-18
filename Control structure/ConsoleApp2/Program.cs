d5e4a9759ef14bf2ef04574aaef34b3ea7451af8
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-08-18 20:20:20 +0400

    Initial commit

---
+++ Control structure/ConsoleApp2/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+int age = 18;
+string gender = "female";
+
+
+
+if (age+5<20)
+{
+    Console.WriteLine("Ishci kimi gelin");
+}
+
+
+
+
+//if (age == 18)
+//{
+//    Console.WriteLine("Stajor");
+//}
+
+
+
+//if (age > 17)
+//{
+//    Console.WriteLine("Ishci kimi gelin");
+//}
+//else if (age == 17)
+//{
+//    Console.WriteLine("Stajor kimi gelin");
+//}
+//else
+//{
+//    Console.WriteLine("Uygun deyilsiz");
+//}
+
+
+
+//if (age > 17)
+//{
+//    if (gender == "female")
+//    {
+//        Console.WriteLine("Ishe qebul olduz");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Xanim ishci axtaririq");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Yashiniz azdir");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int num = 5;
+
+
+
+////num = num + 1;
+////num += 5;  ctrl+k+c   ctrl+k+u
+
+//Console.WriteLine(num+5);
+
+
+
+
+//num = 30;
